d685e337e4b07fc337452db1b6b4491c9d2a8508
Ethan_Shang <shangxia0624@gmail.com>
2018-01-12 18:25:10 -0600

    add files

---
+++ clone.py
@@ -0,0 +1,46 @@
+import csv
+import pdb
+import cv2
+import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+#from sklearn import preprocessing
+#from scipy.ndimage.measurements import label
+
+lines = []
+with open('./driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
+
+pdb.set_trace()
+
+x = 10
+
+print ('c equals to {}'.format(x))
